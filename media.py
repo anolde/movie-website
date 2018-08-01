86a1104377d3f3b57dd24793801c18ab9f21d48e
anolde <sasha.nolde@gmail.com>
2018-08-01 10:45:54 -0500

    Format Changes
    Changes to accomodate pep8 requirements

--- media.py
+++ media.py
@@ -1,9 +1,12 @@
 import webbrowser
 
+
 class Video():
     """This class is the parent to Move and TVShow classes.
-    It contains the properties shared by all media types that can be shared on the site.
+        It contains the properties shared by all media types
+        that can be shared on the site.
     """
+
     def __init__(self, title, plot, poster, release_yr, trailer):
         """Parent Class"""
         # title
@@ -16,27 +19,31 @@ class Video():
         self.yr = release_yr
         # trailer from youtube
         self.trailer_youtube_url = trailer
-    
-    ## Video Methods ##
-    
+
+    # Video Methods
+
     def show_trailer(self):
-        """This function will open the trailer for the instance of the video using webbrowser"""
+        """This function will open the trailer
+        for the instance of the video using webbrowser"""
         webbrowser.open(self.trailer_youtube_url)
-        
+
+
 class Movie(Video):
-    """ This class provides a way to store movie related information. 
-    Takes in the Video parent class. 
+    """ This class provides a way to store movie
+    related information. Takes in the Video parent class.
     Movie attributes:
         rating - rotten tomatoes website score out of 10
     """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    def __init__(self, title, plot, poster, release_yr, trailer, tomatoes_rating):
+
+    def __init__(self, title, plot, poster, release_yr,
+                 trailer, tomatoes_rating):
         """Create a Movie Class Object"""
         Video.__init__(self, title, plot, poster, release_yr, trailer)
         # Rotten Tomatoes movie rating
         self.rating = tomatoes_rating
-   
-        
+
+
 class TvShow(Video):
     """ This class provides a way to store television show related information.
     TvShow attributes:
@@ -45,11 +52,11 @@ class TvShow(Video):
         episode - (str) - the name of the episode you enjoy most
     """
     VALID_RATINGS = ["TV-Y", "TV-Y7", "TV-G", "TV-PG", "TV-14", "TV-MA"]
-    def __init__(self, title, plot, poster, release_yr, season_number, tv_channel, episode_name, trailer):
+
+    def __init__(self, title, plot, poster, release_yr,
+                 season_number, tv_channel, episode_name, trailer):
         """Create a TVShow Class Object"""
         Video.__init__(self, title, plot, poster, release_yr, trailer)
         self.season = season_number
         self.platform = tv_channel
         self.episode = episode_name
-        
-
